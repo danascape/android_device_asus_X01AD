1e56e5d55d96221c04577f3808b7ed073f206de1
Praveen Chavan <pchavan@codeaurora.org>
2016-03-21 09:37:14 -0700

    X01AD: mediacodec: Allow select and eventfd syscalls
    Add pselect6 and eventfd2 syscalls
    
    Bug: 27654762
    
    Change-Id: I9aba945e50bece5b94f4fd15e44bc84d50a41a0b

--- device.mk
+++ device.mk
@@ -280,6 +280,11 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
