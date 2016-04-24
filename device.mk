88d4bc4a848d3257dae0fa57495edaf52c2dfd19
Patrick Lower <devvortex@gmail.com>
2016-04-24 22:58:35 +0100

    X01AD: add doze package
    Taken from yu lettuce
    Includes the following patches
    
    YUDoze: Add service for using proximity sensor for wake-up
    Author: Patrick Lower <devvortex@gmail.com>
    Date:   Mon May 11 16:14:17 2015 -0400
    
    YUDoze: Set gesture to disabled by default
    Author: Arne Coucheron <arco68@gmail.com>
    Date:   Wed May 20 00:56:19 2015 +0200
    
    YUDoze: Consolidate doze settings
    Author: Christopher R. Palmer <crpalmer@gmail.com>
    Date:   Sat May 23 11:42:49 2015 -0700
    
    YUDoze: fix defaults
    Author: Dave Daynard <nardholio@gmail.com>
    Date:   Sat May 23 18:29:13 2015 -0400
    
    YUDoze: Add back button to action bar
    Author: luca020400 <luca.stefani.ge1@gmail.com>
    Date:   Sun May 24 15:47:17 2015 -0700
    
    YUDoze: add proximity wake
    Author: Dave Daynard <nardholio@gmail.com>
    Date:   Mon May 25 12:54:00 2015 -0400
    
    YUDoze: restore proximity wake preference on boot
    Author: Dave Daynard <nardholio@gmail.com>
    Date:   Sat Jul 4 13:44:53 2015 -0400
    
    YUDozeService: Prevent a pm wakeup with a timestamp in the future
    Author: Brandon McAnsh <brandon.mcansh@gmail.com>
    Date:   Fri Jul 24 15:40:45 2015 -0400
    
    YUDozeService: properly fix future timestamp error
    Author: Patrick Lower <devvortex@gmail.com>
    Date:   Wed Aug 5 10:51:03 2015 -0400
    
    YUDoze: fix typo
    Author: Dave Daynard <nardholio@gmail.com>
    Date:   Tue Sep 8 22:56:10 2015 -0400
    
    YUDoze: Use ScreenType util from cmsdk
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Sun Dec 13 02:51:09 2015 -0500
    
    YUDOze: Remove summary for YUDoze
    Author: TheStrix <parthbhatia98@gmail.com>
    Date:   Sun Dec 13 07:25:36 2015 -0500
    
    Change-Id: Ia7e668622eecf4355fd55dc9cda7ca05a6494b93

--- device.mk
+++ device.mk
@@ -211,6 +211,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_ili9881h_720p_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ili9881h_720p_video_mode_dsi_panel.xml \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_inx6301_720p_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_inx6301_720p_video_mode_dsi_panel.xml
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
