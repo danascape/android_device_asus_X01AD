2a23080c9023dfd5a97652e7e9502be4139bf98a
Luca Stefani <luca020400@lineageos.org>
2017-08-24 01:43:10 +0800

    X01AD: Build DRM HIDL HAL interface
    Change-Id: I10d5a1cd3a09be132f56fe59a2667236df3cb594

--- device.mk
+++ device.mk
@@ -97,6 +97,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_ili9881h_720p_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ili9881h_720p_video_mode_dsi_panel.xml \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_inx6301_720p_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_inx6301_720p_video_mode_dsi_panel.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # Framework detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
