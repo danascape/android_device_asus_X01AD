65f452f2314b5eb8a171305160dc0a1da8d7c6b4
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-08 21:56:31 +0200

    X01AD: Include qcom {private,public}/sepolicy
    * Will be used by IMS, CNE and DPM
    
    Change-Id: Iba7506007d19b0f48d67b7fbf409fac6975414f8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,10 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
 # Sepolicy - X01AD
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
+# Sepolicy - Common
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
+
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
