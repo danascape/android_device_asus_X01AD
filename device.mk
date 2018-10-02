540d184ded45b59805014fd0eef408d1e7fdef5b
Bruno Martins <bgcngm@gmail.com>
2018-10-02 21:57:25 +0100

    X01AD: Ship additional textclassifier's smart selection models
    Change-Id: I4de9326fe60a395454834906552518afbc376f49

--- device.mk
+++ device.mk
@@ -59,6 +59,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk_package
