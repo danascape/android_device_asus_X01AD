aaae5c6e665590a40bfde0af9b4cb942239c95fe
shoudil <stephenli@codeaurora.org>
2019-01-09 17:04:11 +0800

    X01AD: Add framework detect modules to PRODUCT_PACKAGES
    Only system variant
    
    Add JNI and libqti_vndfwk_detect module name to product_packages
    to specify module to be installed to device. Here explicitly
    specify both the core and vendor variant in PRODUCT_PACKAGES.
    
    CRs-Fixed: 2372775
    
    Change-Id: I11e6ed62886c0f8e1c9029a54b6ea166be595fb5

--- device.mk
+++ device.mk
@@ -41,6 +41,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
 
+# Framework detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect \
+    libvndfwk_detect_jni.qti
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
