9ca5df00d219744b09d73ff26aa62592a281b5c4
Michael Bestas <mkbestas@lineageos.org>
2019-06-19 02:42:07 +0300

    X01AD: Enforce privapp-permissions whitelist
    * Copy priv-app permissions to product.
    
    Change-Id: Ie9c96ef903737348fa46a939b3db611f9e1e55a0

--- device.mk
+++ device.mk
@@ -347,7 +347,7 @@ PRODUCT_COPY_FILES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # Ramdisk
