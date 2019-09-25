2161250ead88d24393c338517be46bcbbc984d2c
Bruno Martins <bgcngm@gmail.com>
2019-09-25 10:57:01 +0000

    X01AD: Build with VNDK  * VNDK runtime isolation is kept disabled for now because that's also how Xiaomi is shipping P on their devices.
    Change-Id: I43140442ab33f3e9633bb4f347e2bee9be97b447

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,5 +102,9 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
+# Treble
+BOARD_VNDK_RUNTIME_DISABLE := true
+BOARD_VNDK_VERSION := current
+
 # Inherit from the proprietary version
 include vendor/asus/X01AD/BoardConfigVendor.mk
