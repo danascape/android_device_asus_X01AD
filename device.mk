e2cb07039eadc4c44eaefb2bed0da8509f89cdf1
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-28 22:13:48 +0530

    X01AD: Build camera hidl hals
    Change-Id: Ic87594e946c41413e8bfe5e7c3c37b67b0fed28a

--- device.mk
+++ device.mk
@@ -156,7 +156,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-   libstdc++.vendor
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5 \
+    libstdc++.vendor
 
 PRODUCT_PACKAGES += \
     Snap
