5b33854b3f202c096424a114cb5566efd94f0b6d
AndrielFR <andrielkogama2@gmail.com>
2020-10-26 00:12:32 -0300

    X01AD: Build vendor.qti.hardware.camera.device@1.0
    Signed-off-by: AndrielFR <andrielkogama2@gmail.com>

--- device.mk
+++ device.mk
@@ -168,6 +168,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++.vendor
 
 PRODUCT_PACKAGES += \
