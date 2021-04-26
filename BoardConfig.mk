grapheneOS-extended/device_asus_X01AD

X01AD: Fix typo in console kernel parameter
Change-Id: Ia38914588a0b0e3bc30c51df9a7b8681f5bf9d76

BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_BOOTLOADER_BOARD_NAME := SDM632
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3

danascape/android_device_asus_X01AD

BoardConfig.mk
@@ -34,7 +34,7 @@ TARGET_BOOTLOADER_BOARD_NAME := SDM632
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1