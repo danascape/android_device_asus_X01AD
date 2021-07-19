b9d729d85cdd7afba093dcb0ae0aa1357072a100
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-19 16:07:34 +0200

    X01AD: Downrev soundtrigger to 2.1
    * Soundtrigger HAL doesn't really like new HIDL versions
    
    Change-Id: I5503753d4ba35a81e0e84963faeb1deaf1ee2223

--- device.mk
+++ device.mk
@@ -107,7 +107,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
