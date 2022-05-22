573a812e46a2cc97ecb01d3a1a89bcf8b7f2c3cc
Nishant <82361039+Ultron12bot@users.noreply.github.com>
2022-05-22 06:39:00 +0530

    Update device.mk
    Drm fixed

--- device.mk
+++ device.mk
@@ -210,10 +210,10 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.0-impl:64 \
+    android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # FM
 PRODUCT_PACKAGES += \
