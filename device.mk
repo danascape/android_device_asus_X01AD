0a82577a0a61895ce564ba990633d70e9c827b44
Saalim Quadri <danascape@gmail.com>
2023-06-10 18:19:54 +0530

    X01AD: Drop deprecated libcnefeatureconfig
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -169,10 +169,6 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-# CNE
-PRODUCT_PACKAGES += \
-    libcnefeatureconfig
-
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
